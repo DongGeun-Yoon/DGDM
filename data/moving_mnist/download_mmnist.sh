ed79e62301fde368c2c7e437c4105367cf310c97
DongGeun-Yoon <59082794+DongGeun-Yoon@users.noreply.github.com>
2024-11-06 13:36:24 +0900

    Update download_mmnist.sh

--- data/moving_mnist/download_mmnist.sh
+++ data/moving_mnist/download_mmnist.sh
@@ -1,5 +1,5 @@
-wegt https://github.com/tychovdo/MovingMNIST/raw/master/mnist_test_seq.npy.gz
-wegt https://github.com/fgnt/mnist/blob/master/train-images-idx3-ubyte.gz
+wget https://github.com/tychovdo/MovingMNIST/raw/master/mnist_test_seq.npy.gz
+wget https://github.com/fgnt/mnist/blob/master/train-images-idx3-ubyte.gz
 
 # wget http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy
 # wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
